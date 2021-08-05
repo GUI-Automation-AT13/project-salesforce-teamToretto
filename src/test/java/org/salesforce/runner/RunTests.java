a5f6748422a33e59f36c8ee10007a84c7eca31bc
cristianfchq <cristian.fchq@gmail.com>
2021-08-05 12:50:16 -0400

    add pre and post conditions

--- src/test/java/org/salesforce/runner/RunTests.java
+++ src/test/java/org/salesforce/runner/RunTests.java
@@ -1,11 +1,32 @@
 package org.salesforce.runner;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import core.api.ApiManager;
+import core.api.ApiMethod;
+import core.api.ApiRequest;
+import core.api.ApiResponse;
+import core.utils.PropertiesReader;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpStatus;
 import org.apache.log4j.Logger;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
+import salesforce.api.ApiEndPoints;
+import salesforce.api.Authentication;
+import salesforce.api.entities.Account;
+import salesforce.api.entities.Contact;
+import salesforce.api.entities.Response;
+
+import static salesforce.api.Authentication.token;
+import static io.restassured.RestAssured.authentication;
+
+import java.util.Properties;
 
 @CucumberOptions(
         features = {"src/test/resources/features"},
@@ -14,17 +35,90 @@ import org.testng.annotations.DataProvider;
 )
 public class RunTests extends AbstractTestNGCucumberTests {
     public Logger LOGGER = Logger.getLogger(getClass());
+    public ApiRequest apiRequest;
+    public ApiResponse apiResponse;
+    public Authentication authentication = new Authentication();
+    public Account account;
+    public Contact contact;
     @Override
     @DataProvider(parallel = true)
     public Object[][] scenarios(){
         return super.scenarios();
     }
     @BeforeTest
-    public void beforeExecution() {
-        LOGGER.info("----------- Before Execution -----------");
+    public void loginAndSetup() throws JsonProcessingException {
+        apiRequest = new ApiRequest();
+        Properties properties = PropertiesReader.getProperties("config.properties");
+        authentication.getAuth();
+        apiRequest.addHeader("Content-Type", "application/json")
+                .addHeader(HttpHeaders.AUTHORIZATION, token.getTokenType() + " " + token.getAccessToken())
+                .setBaseUri(token.getInstanceUrl() + properties.getProperty("SERVICE")
+                        + properties.getProperty("VERSION"));
+        createAccount();
+        createContact();
     }
     @AfterTest
     public void afterExecution() {
-        LOGGER.info("----------- After Execution -----------");
+        deleteAccount();
+        deleteContact();
+        apiRequest = new ApiRequest();
+    }
+
+    public void deleteAccount() {
+        apiRequest.clearPathParam();
+        apiRequest.method(ApiMethod.DELETE)
+                .endpoint(ApiEndPoints.ACCOUNT_ID)
+                .addPathParam("ACCOUNT_ID", account.getId());
+
+        apiResponse = new ApiResponse();
+        ApiManager.execute(apiRequest, apiResponse);
+        Assert.assertEquals(apiResponse.getStatusCode(), HttpStatus.SC_NO_CONTENT);
+        apiResponse.getResponse().then().log().body();
+    }
+
+    public void deleteContact() {
+        apiRequest.clearPathParam();
+        apiRequest.method(ApiMethod.DELETE)
+                .endpoint(ApiEndPoints.CONTACT_ID)
+                .addPathParam("CONTACT_ID", contact.getId());
+
+        apiResponse = new ApiResponse();
+        ApiManager.execute(apiRequest, apiResponse);
+        Assert.assertEquals(apiResponse.getStatusCode(), HttpStatus.SC_NO_CONTENT);
+        apiResponse.getResponse().then().log().body();
+    }
+//    @BeforeTest
+//    public void beforeExecution() {
+//        LOGGER.info("----------- Before Execution -----------");
+//    }
+//    @AfterTest
+//    public void afterExecution() {
+//        LOGGER.info("----------- After Execution -----------");
+//    }
+    public void createAccount() throws JsonProcessingException {
+        account = new Account();
+        account.setName("TestAccount");
+        apiRequest.method(ApiMethod.POST)
+                .endpoint(ApiEndPoints.ACCOUNT)
+                .body(new ObjectMapper().writeValueAsString(account));
+        apiResponse = new ApiResponse();
+        ApiManager.execute(apiRequest, apiResponse);
+        Assert.assertEquals(apiResponse.getStatusCode(), HttpStatus.SC_CREATED);
+        apiResponse.getResponse().then().log().body();
+        account.setId(apiResponse.getBody(Response.class).getId());
+    }
+
+    public void createContact() throws JsonProcessingException {
+        contact = new Contact();
+        contact.setLastName("TestContact");
+        contact.setEmail("jala@mail.com");
+        apiRequest.method(ApiMethod.POST)
+                .endpoint(ApiEndPoints.CONTACT)
+                .body(new ObjectMapper().writeValueAsString(contact));
+        apiResponse = new ApiResponse();
+        ApiManager.execute(apiRequest, apiResponse);
+        Assert.assertEquals(apiResponse.getStatusCode(), HttpStatus.SC_CREATED);
+        apiResponse.getResponse().then().log().body();
+        contact.setId(apiResponse.getBody(Response.class).getId());
     }
 }
--- src/test/resources/features/createCampaign.feature
+++ src/test/resources/features/createCampaign.feature
@@ -13,7 +13,7 @@ Feature: Create Campaign
     Given I navigate to the "Campaign" page
     When I create a new campaign with all fields
       | Campaign Name                 | New Campaign          |
-      | Start Date                    | 8/15/2021             |
+      | Start Date                    | 7/15/2021             |
       | End Date                      | 8/18/2021             |
       | Expected Revenue in Campaign  | 200                   |
       | Budgeted Cost in Campaign     | 150                   |
