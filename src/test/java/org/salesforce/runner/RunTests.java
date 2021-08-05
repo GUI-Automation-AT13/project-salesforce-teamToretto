81cbd43ce6e4dc89e3369569f3c3f44481aee0d4
cristianfchq <cristian.fchq@gmail.com>
2021-08-05 11:57:33 -0400

    adds functions of beforetest an after test

--- src/test/java/org/salesforce/runner/RunTests.java
+++ src/test/java/org/salesforce/runner/RunTests.java
@@ -2,6 +2,9 @@ package org.salesforce.runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.apache.log4j.Logger;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
@@ -10,9 +13,18 @@ import org.testng.annotations.DataProvider;
         glue = {"org.salesforce"}
 )
 public class RunTests extends AbstractTestNGCucumberTests {
+    public Logger LOGGER = Logger.getLogger(getClass());
     @Override
     @DataProvider(parallel = true)
     public Object[][] scenarios(){
         return super.scenarios();
+    }
+    @BeforeTest
+    public void beforeExecution() {
+        LOGGER.info("----------- Before Execution -----------");
+    }
+    @AfterTest
+    public void afterExecution() {
+        LOGGER.info("----------- After Execution -----------");
     }
 }
