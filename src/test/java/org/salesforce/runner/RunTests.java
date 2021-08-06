96121bf49b42a131f5a49d49dc663ba1743b78ea
cristianfchq <cristian.fchq@gmail.com>
2021-08-05 20:20:21 -0400

    solves comments of reviwers

--- src/test/java/org/salesforce/runner/RunTests.java
+++ src/test/java/org/salesforce/runner/RunTests.java
@@ -10,13 +10,10 @@ import core.utils.PropertiesReader;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.apache.http.HttpHeaders;
-import org.apache.http.HttpStatus;
 import org.apache.log4j.Logger;
-import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.AfterTest;
 import salesforce.api.ApiEndPoints;
 import salesforce.api.Authentication;
 import salesforce.api.entities.Account;
@@ -24,7 +21,6 @@ import salesforce.api.entities.Contact;
 import salesforce.api.entities.Response;
 
 import static salesforce.api.Authentication.token;
-import static io.restassured.RestAssured.authentication;
 
 import java.util.Properties;
 
@@ -36,17 +32,20 @@ import java.util.Properties;
 public class RunTests extends AbstractTestNGCucumberTests {
     public Logger LOGGER = Logger.getLogger(getClass());
     public ApiRequest apiRequest;
-    public ApiResponse apiResponse;
     public Authentication authentication = new Authentication();
+    public ApiResponse apiResponse;
     public Account account;
     public Contact contact;
+
     @Override
-    @DataProvider(parallel = true)
+    @DataProvider(parallel = false)
     public Object[][] scenarios(){
         return super.scenarios();
     }
+
     @BeforeTest
     public void loginAndSetup() throws JsonProcessingException {
+        LOGGER.info("----------- Login And Setup -----------");
         apiRequest = new ApiRequest();
         Properties properties = PropertiesReader.getProperties("config.properties");
         authentication.getAuth();
@@ -54,42 +53,39 @@ public class RunTests extends AbstractTestNGCucumberTests {
                 .addHeader(HttpHeaders.AUTHORIZATION, token.getTokenType() + " " + token.getAccessToken())
                 .setBaseUri(token.getInstanceUrl() + properties.getProperty("SERVICE")
                         + properties.getProperty("VERSION"));
-        createAccount();
-        createContact();
     }
 
-    @AfterTest
+    @AfterTest(dependsOnMethods = {"deleteAccount", "deleteContact"})
     public void afterExecution() {
-        deleteAccount();
-        deleteContact();
         apiRequest = new ApiRequest();
     }
 
+    @AfterTest
     public void deleteAccount() {
+        LOGGER.info("----------- Delete a Account feature -----------");
         apiRequest.clearPathParam();
         apiRequest.method(ApiMethod.DELETE)
                 .endpoint(ApiEndPoints.ACCOUNT_ID)
                 .addPathParam("ACCOUNT_ID", account.getId());
-
         apiResponse = new ApiResponse();
         ApiManager.execute(apiRequest, apiResponse);
-        Assert.assertEquals(apiResponse.getStatusCode(), HttpStatus.SC_NO_CONTENT);
         apiResponse.getResponse().then().log().body();
     }
-
+    @AfterTest(dependsOnMethods = "deleteAccount")
     public void deleteContact() {
+        LOGGER.info("----------- Delete a Contact feature -----------");
         apiRequest.clearPathParam();
         apiRequest.method(ApiMethod.DELETE)
                 .endpoint(ApiEndPoints.CONTACT_ID)
                 .addPathParam("CONTACT_ID", contact.getId());
-
         apiResponse = new ApiResponse();
         ApiManager.execute(apiRequest, apiResponse);
-        Assert.assertEquals(apiResponse.getStatusCode(), HttpStatus.SC_NO_CONTENT);
         apiResponse.getResponse().then().log().body();
     }
 
+    @BeforeTest(dependsOnMethods = "loginAndSetup")
     public void createAccount() throws JsonProcessingException {
+        LOGGER.info("----------- Create a Account feature -----------");
         account = new Account();
         account.setName("TestAccount");
         apiRequest.method(ApiMethod.POST)
@@ -97,12 +93,13 @@ public class RunTests extends AbstractTestNGCucumberTests {
                 .body(new ObjectMapper().writeValueAsString(account));
         apiResponse = new ApiResponse();
         ApiManager.execute(apiRequest, apiResponse);
-        Assert.assertEquals(apiResponse.getStatusCode(), HttpStatus.SC_CREATED);
         apiResponse.getResponse().then().log().body();
         account.setId(apiResponse.getBody(Response.class).getId());
     }
 
+    @BeforeTest(dependsOnMethods = "loginAndSetup")
     public void createContact() throws JsonProcessingException {
+        LOGGER.info("----------- Create a Contact feature -----------");
         contact = new Contact();
         contact.setLastName("TestContact");
         contact.setEmail("jala@mail.com");
@@ -111,7 +108,6 @@ public class RunTests extends AbstractTestNGCucumberTests {
                 .body(new ObjectMapper().writeValueAsString(contact));
         apiResponse = new ApiResponse();
         ApiManager.execute(apiRequest, apiResponse);
-        Assert.assertEquals(apiResponse.getStatusCode(), HttpStatus.SC_CREATED);
         apiResponse.getResponse().then().log().body();
         contact.setId(apiResponse.getBody(Response.class).getId());
     }
