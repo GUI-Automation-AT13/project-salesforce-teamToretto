f3033b6cb7bd6d40345a03ef7165c36e2fd6f5c8
SaulCM <saulcaspamiranda@gmail.com>
2021-08-10 09:46:59 -0400

    fixed delete individual pos condition

--- src/test/java/org/salesforce/hooks/IndividualScenarioHooks.java
+++ src/test/java/org/salesforce/hooks/IndividualScenarioHooks.java
@@ -10,12 +10,16 @@ package org.salesforce.hooks;
 
 import core.selenium.WebDriverManager;
 import io.cucumber.java.After;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
 import org.testng.asserts.SoftAssert;
 import salesforce.ui.PageTransporter;
 import salesforce.ui.pages.lightning.individuals.IndividualListPage;
 
 public class IndividualScenarioHooks {
 
+    private Logger log = LogManager.getLogger(getClass());
     private WebDriverManager webDriverManager;
     private PageTransporter pageTransporter;
     private SoftAssert softAssert;
@@ -26,8 +30,12 @@ public class IndividualScenarioHooks {
     }
 
     @After(value = "@CreateIndividual")
-    public void setLast() {
+    public void deleteIndividual() {
+        log.info("Delete Individual");
         IndividualListPage individualListPage = pageTransporter.navigateToIndividualListPage();
         individualListPage.deleteLastModifiedRecord();
+        String actual = individualListPage.getDeletedSuccessMessage();
+        String expected = "was deleted.";
+        Assert.assertTrue(actual.contains(expected));
     }
 }
